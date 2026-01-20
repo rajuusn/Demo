4088a2ee142bd652d47fa530d1640c4389b10d39
rajuusn <rajuusn999@gmail.com>
2026-01-20 12:36:23 +0530

    new commit

--- src/main/java/pageObjects/New.java
+++ src/main/java/pageObjects/New.java
@@ -7,7 +7,7 @@ public class New {
 	public static void main(String[] args) {
 		String st="new java file added from git hub";
 		System.out.print(st);
-System.out.println("seco");
+System.out.println("second");
 	}
 
 }
